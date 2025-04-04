fa6929a771c575156ace294e5de1ee5a9a3b9b75
Zichang Reyner Liu <146147400+reynerliu@users.noreply.github.com>
2025-04-05 07:39:00 +0800

    Update main.py
    border修改

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import argparse
 from PIL import Image
 import rawpy
 from metadata import get_nef_metadata
-from border import create_bottom_border_canvas
+from border import create_bottom_border_canvas,create_blurred_background,create_border
 from compose import *
 
 def load_image(image_path):
@@ -21,6 +21,7 @@ if __name__ == "__main__":
     parser.add_argument("nef_path", type=str, help="Path to input NEF file")
     parser.add_argument("output_path", type=str, help="Path to save output image")
     parser.add_argument("--author", type=str, default="Harlan", help="Author name (optional)")
+    parser.add_argument("--border", type=str, default="basic", help="blur or basic border")
     args = parser.parse_args()
 
     script_dir = os.path.dirname(os.path.abspath(__file__))
@@ -28,19 +29,21 @@ if __name__ == "__main__":
     nef_path = args.nef_path
     output_path = args.output_path
     author = args.author
+    border = args.border
 
     img = load_image(nef_path)
     meta = get_nef_metadata(nef_path)
     print("Metadata:", meta)
 
-    img_with_border = create_bottom_border_canvas(img)
+    #img_with_border = create_bottom_border_canvas(img)
+    img_with_border = create_border(img,border)
 
     # 判断图片方向
     if img_with_border.width > img_with_border.height:
         final_img = compose_photo_card_horizon(img_with_border, meta, nikon_logo_path="logos/Nikon_logo.png",
-                                               author=author)
+                                               author=author,border=border)
     else:
         final_img = compose_photo_card_vertical(img_with_border, meta, nikon_logo_path="logos/Nikon_logo.png",
-                                                author=author)
+                                                author=author,border=border)
     final_img.save(output_path)
     print(f"✅ Saved with bottom border: {output_path}")
